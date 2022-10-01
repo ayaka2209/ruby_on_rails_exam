2b6aedd865017c2bfb95fd062d0a168879de8c1a
ayaka tooya <na081543@icloud.com>
2022-10-01 12:37:30 +0900

    削除機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -20,10 +21,31 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"つぶやきを削除しました！"
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "つぶやきました！"
+      else
+        render :edit
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
